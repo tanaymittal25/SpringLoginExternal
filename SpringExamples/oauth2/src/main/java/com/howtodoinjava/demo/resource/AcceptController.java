1353211f2eadaad73d9114afc6e86e8c44f7bf60
tanaymittal25 <tanay.mittal25@gmail.com>
2020-02-14 12:19:56 +0530

    Populate

--- SpringExamples/oauth2/src/main/java/com/howtodoinjava/demo/resource/AcceptController.java
+++ SpringExamples/oauth2/src/main/java/com/howtodoinjava/demo/resource/AcceptController.java
@@ -1,14 +1,22 @@
 package com.howtodoinjava.demo.resource;
 
+import java.io.Serializable;
 import java.security.Principal;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.web.servlet.View;
 import com.howtodoinjava.demo.resource.AuthorizationEndpoint;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.common.exceptions.UnsupportedResponseTypeException;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
 import org.springframework.security.oauth2.provider.AuthorizationRequest;
@@ -29,36 +37,67 @@ public class AcceptController extends AuthorizationRequest {
 	    return "redirect:" + redirectConsentPage;
 	}
 	
-	@SuppressWarnings("null")
 	@RequestMapping("/authorize-success")
 	public View authCode() {
-		
-		AuthorizationRequest authReq = null;
-		
 		
 		Map<String, String> approvalParameters = new HashMap<String, String>();
 		approvalParameters.put("user_oauth_approval", "true");
 		approvalParameters.put("scope.read_profile_info", "true");
 		approvalParameters.put("authorize", "Authorize");
-		authReq.setApprovalParameters(approvalParameters);
+		//authReq.setApprovalParameters(approvalParameters);
 		
-		authReq.setRedirectUri("https://www.volantetech.com/");
-		authReq.setClientId("clientapp");
+		//authReq.setRedirectUri("https://www.volantetech.com/");
+		//authReq.setClientId("clientapp");
 		
-		List<String> listScope = Arrays.asList("read_profile_info");
-		authReq.setScope(listScope);
+		Set<String> scope = new HashSet<String>();
+		//authReq.setScope(listScope);
+		scope.add("read_profile_info");
 		
 		Map<String, String> requestParameters = new HashMap<String, String>();
 		requestParameters.put("response_type", "code");
 		requestParameters.put("client_id", "clientapp");
 		requestParameters.put("scope", "read_profile_info");
-		authReq.setRequestParameters(requestParameters);
+		//authReq.setRequestParameters(requestParameters);
 		
-		Authentication authUser = null;
-		authUser.setAuthenticated(true);
+		Set<String> responseTypes = new HashSet<String>();
+		responseTypes.add("code");
+		//authReq.setResponseTypes(responseTypes);
+		
+		Set<String> resourceIds = new HashSet<String>();
+		resourceIds.add("oauth2-resource");
+		//authReq.setResourceIds(resourceIds);
+		
+		//authReq.setApproved(true);
+		
+		//Map<String,Serializable> extensions = new HashMap<String,Serializable>();
+		//authReq.setExtensions(extensions);
+		
+		//String state;
+		//authReq.setState("null");
+		
+		Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>(); 
+		SimpleGrantedAuthority simpleAuth = new SimpleGrantedAuthority("READ_ONLY_CLIENT");
+		authorities.add(simpleAuth);
+		//authReq.setAuthorities(authorities);
+		
+		AuthorizationRequest authReq = new AuthorizationRequest(requestParameters, approvalParameters, "clientapp", scope, resourceIds, authorities, true, "null", "https://www.volantetech.com/",
+		        responseTypes);
+		Map<String,Serializable> extensions = new HashMap<String,Serializable>();
+		authReq.setExtensions(extensions);
+		
+		SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
+		
+		Set<GrantedAuthority> authoritiesPrincipal = new HashSet<GrantedAuthority>();
+		authoritiesPrincipal.add(simpleGrantedAuthority);
+		User user = new User("volante", "123456", true, true, true, true, authoritiesPrincipal);
+		
+		UsernamePasswordAuthenticationToken authUser = new UsernamePasswordAuthenticationToken(user, null, authoritiesPrincipal);
+		
+		//org.springframework.security.core.userdetails.User
+		//Authentication authUser = null;
 		
 		
-		return AuthorizationEndpoint.getAuthorizationCodeResponse(authReq, authUser);
+		return AuthorizationEndpoint.getAuthorizationCodeResponse(authReq, (Authentication) authUser);
 		
 	}
 	
